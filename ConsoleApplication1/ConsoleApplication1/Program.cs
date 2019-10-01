e68aeb0d6a6699e7d2f301833ad432a334d007f2
Vu Cong Anh <conganh270197@gmail.com>
2019-10-01 16:08:14 +0700

    hello

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github");
+            Console.WriteLine(isPrime(6));
+
+            //Console.WriteLine("Hello Github");
+            // Console.WriteLine(isPrime(6));
+            // Console.WriteLine(Ceil((float)12.8));
+            // Console.WriteLine(Floor((float)12.5));
+            Console.WriteLine(Rand1());
+            Console.WriteLine(Rand2());
+            Console.WriteLine(Factorial(5));
+
+        }
+        public static bool isOdd(int n)
+        {
+            if (n % 2 == 1)
+                return true;
+            return false;
+        }
+        public static bool isEven(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            return false;
+        }
+        public static bool isPrime(int n)
+        {
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    count++;
+            }
+            if (count == 2)
+                return true;
+            return false;
+        }
+        public static int Square(int n)
+        {
+            return n * n;
+        }
+        public static int Cube(int n)
+        {
+            return n * n * n;
+        }
+        public static int Pow(int x, int y)
+        {
+            int R = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                R = R * x;
+            }
+            return R;
+        }
+        public static int Abs(int n)
+        {
+            if (n > 0)
+                n = n;
+            else
+                n = n * (-1);
+            return n;
+        }
+        //returns the smallest integral value not less than x.
+        public static int Ceil(float x)
+        {
+            return (int)x + 1;
+        }
+        public static int Floor(float x)
+        {
+            return (int)x;
+        }
+        public static int Rand1()
+        {
+            Random rand = new Random();
+
+            int rd = rand.Next(0, 2147483647);
+            return rd;
+        }
+        public static float Rand2()
+        {
+            Random rand = new Random();
+            var n = rand.NextDouble();
+            return (float)n;
+        }
+        public static long Factorial(long n)
+        {
+            if (n == 1)
+                return n = 1;
+
+            return Factorial(n - 1) * n;
+
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsOdd()
+        {
+            bool result3 = Program.isOdd(3);
+            Assert.AreEqual(true, result3);
+            bool result4 = Program.isOdd(4);
+            Assert.AreEqual(false, result4);
+        }
+        [TestMethod]
+        public void TestIsEven()
+        {
+            bool result3 = Program.isEven(3);
+            Assert.AreEqual(false, result3);
+            bool result4 = Program.isEven(4);
+            Assert.AreEqual(true, result4);
+        }
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            bool result7 = Program.isPrime(7);
+            Assert.AreEqual(true, result7);
+            bool result6 = Program.isPrime(6);
+            Assert.AreEqual(false, result6);
+        }
+        [TestMethod]
+        public void TestSquare()
+        {
+            int result4 = Program.Square(4);
+            Assert.AreEqual(16, result4);
+        }
+        [TestMethod]
+        public void TestCube()
+        {
+            int result4 = Program.Cube(4);
+            Assert.AreEqual(64, result4);
+        }
+        [TestMethod]
+        public void TestPow()
+        {
+            int result4 = Program.Pow(2, 5);
+            Assert.AreEqual(32, result4);
+        }
+        [TestMethod]
+        public void TestAbs()
+        {
+            int result = Program.Abs(4);
+            Assert.AreEqual(4, result);
+        }
+        [TestMethod]
+        public void TestCeil()
+        {
+            float result = Program.Ceil((float)3.5);
+            Assert.AreEqual(4, result);
+        }
+        [TestMethod]
+        public void TestFloor()
+        {
+            float result = Program.Floor((float)3.5);
+            Assert.AreEqual(3, result);
+        }
+        [TestMethod]
+        public void TestFactorial()
+        {
+            long result = Program.Factorial(5);
+            Assert.AreEqual(120, result);
+        }
+    }
+}
